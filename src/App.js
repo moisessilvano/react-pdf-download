1f4864f285555f16c1ec2bbfd5794521e07659ae
Moisés Barbosa <moises.barbosa@iteris.com.br>
2022-09-05 14:52:05 -0300

    ajuste

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => download()}>Download</button>
+      <button onClick={() => download()}>BAIXAR PDF</button>
     </div>
   );
 }
